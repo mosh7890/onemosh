4e1bf8a41698a618d6242829c551289a5106e7ac
mosh7890 <mosh7890@gmail.com>
2019-04-06 18:07:55 +0300

    DB Settings

--- src/onemosh/settings/production.py
+++ src/onemosh/settings/production.py
@@ -6,24 +6,23 @@ DEBUG = False
 # environment variable in ebs config
 ALLOWED_HOSTS = config('ALLOWED_HOSTS_PRODUCTION', cast=Csv())
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': config('PYTHONANYWHERE_USERNAME') + '$' + config('PYTHONANYWHERE_DB_NAME'),
-            'USER': config('PYTHONANYWHERE_USERNAME'),
-            'PASSWORD': config('PYTHONANYWHERE_PW'),
-            'HOST': config('PYTHONANYWHERE_HOSTNAME'),
-            'OPTIONS': {
-                'charset': 'utf8mb4',
-            },
-            'TEST': {
-                'NAME': config('PYTHONANYWHERE_USERNAME') + '$test_' + config('PYTHONANYWHERE_DB_NAME'),
-                'CHARSET': 'utf8mb4',
-                'COLLATION': 'utf8mb4_unicode_ci',
-            },
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('PYTHONANYWHERE_USERNAME') + '$' + config('PYTHONANYWHERE_DB_NAME'),
+        'USER': config('PYTHONANYWHERE_USERNAME'),
+        'PASSWORD': config('PYTHONANYWHERE_PW'),
+        'HOST': config('PYTHONANYWHERE_HOSTNAME'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'TEST': {
+            'NAME': config('PYTHONANYWHERE_USERNAME') + '$test_' + config('PYTHONANYWHERE_DB_NAME'),
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        },
     }
+}
 
 # drf
 DRF_NEW_ENTRY = {
